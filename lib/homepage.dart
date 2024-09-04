ae096cd37179babc1aacdc81c2e1136b63105897
jonabergamo <jonathanbergamo16@gmail.com>
2024-09-04 03:08:58 -0300

    feat: first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: Container(
+      color: Colors.black,
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+        child: GNav(
+          backgroundColor: Colors.black,
+          color: Colors.white,
+          activeColor: Colors.white,
+          tabBackgroundColor: Colors.white10,
+          tabBorderRadius: 15,
+          gap: 8,
+          padding: EdgeInsets.all((16)),
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.favorite_border,
+              text: 'Likes',
+            ),
+            GButton(
+              icon: Icons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: 'Settings',
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
